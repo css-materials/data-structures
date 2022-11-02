01731bd01eddd4e54c96cd7339d1e0aee4cadc89
Sabrina Nardin <nardin@uchicago.com>
2022-11-02 17:40:14 -0500

    Expanded and added more details to examples

--- vectors-lists-solutions.R
+++ vectors-lists-solutions.R
@@ -6,6 +6,9 @@
 # given the following atomic vector
 (x <- seq(from = 1, to = 10))
 
+# check its structure
+str(x)
+
 # keep the first element
 x[1]
 
@@ -27,7 +30,8 @@ x[c(-1,2,3)]  # error! do not mix negative and positive subscripts
 
 # keep all elements with values greater than five
 x > 5       # first, get the index for which values x > 5
-x[x > 5]    # then, use to subset the vector
+x[x > 5]    # then, use to subset the vector 
+# nb: you only need line 30, line 29 is only to show what that code does
 
 # keep all even elements (use modular division)
 x[x %% 2 == 0]  
@@ -36,27 +40,55 @@ x[x %% 2 == 0]
 ## 2. SUBSETTING A LIST 
 
 # given the following list
-(y <- list(a = c(1, 2, 3), 
+(y <- list(a = c(4,5,6), 
            b = "today is a beautiful day", 
            c = matrix(c(9,8,1,2), nrow=2), 
-           d = list(-1, -5)))
+           d = list(-1,-5)))
 
-# use [] to access "a"
-# results should be a list
-y[1]    # by position
-y["a"]  # by name
-y[2]    # by position
-y["b"]  # by name
+# check its structure
+str(y)
 
-# use [[]] to access "a"
-# result should be an atomic vector
-y[[1]]   # by position
-y[["a"]] # by name
+# use [] to access "a" -- results should be a list
+y[1]         # by position
+y["a"]       # by name
+str(y[1])
 
-# combine [[]] and [] to access specific elements values
-y[[1]][1]
-y[["a"]][1]
+# use [] to access "d" -- results should be a nested list
+y[4]         # by position
+y["d"]       # by name
+str(y[4])
 
-## subset the first and third elements from y
-y[c(1, 3)]
-y[c("a", "c")]
+# use [[]] to access "a" -- results should be an atomic vector
+y[[1]]       # by position
+y[["a"]]     # by name
+str(y[[1]])
+
+# use [[]] to access "d" -- results should be a list
+y[[4]]       # by position
+y[["d"]]     # by name
+str(y[[4]])
+
+# combine [[]] and [] to access specific elements values:
+
+# access the second element from "a" which is the number 5
+y[[1]][2]    # by position
+y[["a"]][2]  # by name
+
+# access the second element from "d" which is -5
+y[[4]][2]    # by position
+y[["d"]][2]  # by name
+
+# notice that -5 is still a list 
+# because "d" is originally a list (nested within the list y)
+str(y[["d"]][2])
+
+# to access -5 as an atomic vector, use double [[]] [[]] 
+# see visual example in the slides
+y[[4]][[2]]    # by position
+y[["d"]][[2]]  # by name
+
+# subset the first and third elements from y
+y[c(1, 3)]      # by position
+y[c("a", "c")]  # by name
+# for more practice, further subset these elements using 
+# [[]] and combining [[]] and []
--- vectors-lists.R
+++ vectors-lists.R
@@ -1,49 +1,58 @@
 # data structure - practice
 # review also lecture 2 "intro to R" and exercises
 
-
 ## 1. SUBSETTING A VECTOR 
 
 # given the following atomic vector
 (x <- seq(from = 1, to = 10))
 
-# keep the first element
+# check its structure
+str(x)
 
+# keep the first element
 
 # keep the first through fourth elements
 
-
 # keep the first through fourth elements, plus the seventh element
-
 
 # keep all elements beside the first three
 
-
 # keep all elements with values greater than five
 
-
 # keep all even elements (use modular division)
-
 
 
 ## 2. SUBSETTING A LIST 
 
 # given the following list
-(y <- list(a = c(1, 2, 3), 
+(y <- list(a = c(4,5,6), 
            b = "today is a beautiful day", 
            c = matrix(c(9,8,1,2), nrow=2), 
-           d = list(-1, -5)))
+           d = list(-1,-5)))
 
-# use [] to access "a"
-# results should be a list
+# check its structure
+str(y)
+
+# use [] to access "a" -- results should be a list
+
+# use [] to access "d" -- results should be a nested list
+
+# use [[]] to access "a" -- results should be an atomic vector
+
+# use [[]] to access "d" -- results should be a list
 
 
-# use [[]] to access "a"
-# result should be an atomic vector
+# combine [[]] and [] to access specific elements values:
 
 
-# combine [[]] and [] to access specific elements values
+# access the second element from "a" which is the number 5
 
 
-## subset the first and third elements from y
+# access the second element from "d" which is -5
 
+
+# notice that -5 is still a list 
+# to access -5 as an atomic vector, use double [[]] [[]] 
+
+
+# subset the first and third elements from y
